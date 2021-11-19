abaa40d5aed767eeaf12d4ae27555705277d2650
Samuel <s.weber@webu.coop>
2021-11-19 17:10:34 +0100

    add calendar name + try to guess if nextcloud agenda

--- dalec_caldav/proxy.py
+++ dalec_caldav/proxy.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from typing import Dict
 import requests
+from urllib.parse import urlparse
 
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import now
@@ -8,7 +9,7 @@ from django.conf import settings
 
 from dalec.proxy import Proxy
 
-from caldav import DAVClient
+from caldav import DAVClient, dav
 
 client = DAVClient(
         url=settings.DALEC_CALDAV_BASE_URL,
@@ -41,6 +42,8 @@ class CaldavProxy(Proxy):
                 }
 
         if channel == "url":
+            if channel_object[-1] != "/":
+                channel_object += "/"
             calendars = client.calendar(url=channel_object)
             if type(calendars) != list:
                 calendars = [calendars]
@@ -50,6 +53,9 @@ class CaldavProxy(Proxy):
 
         contents = {}
         for calendar in calendars:
+            calendar_displayname = calendar.get_property(dav.DisplayName())
+            calendar_url = calendar.url.url_raw
+
             for event in calendar.events():
                 id = str(event.vobject_instance.vevent.contents["uid"][0].value)
                 content = {
@@ -58,7 +64,17 @@ class CaldavProxy(Proxy):
                         in event.vobject_instance.vevent.contents.items()
                         }
                 content["event_url"] = str(event.url)
-                content["dav_calendar_url"] = calendar.url.url_raw
+                content["dav_calendar_url"] = calendar_url
+                content["calendar_displayname"] = calendar_displayname
+
+                res = urlparse(calendar.url.url_raw)
+                if ( "/remote.php/dav/public-calendars/" in res.path):
+                    # seems to be nextcloud
+                    token = res.path.split("/")[-1]
+                    nextcloud_calendar_url = "{}://{}/index.php/apps/calendar/p/{}".format(
+                        res.scheme, res.netloc, token
+                    )
+                    content["nextcloud_calendar_url"] = nextcloud_calendar_url
 
                 contents[id] = {
                         **content,
