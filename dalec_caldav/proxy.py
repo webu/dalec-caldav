ebd32b4ef4e9c85a9d3b6e98561de1b2cc82ba28
Dylann Cordel <cordel.d@free.fr>
2022-01-20 09:31:22 +0100

    release 0.0.0-rc.0

--- dalec_caldav/proxy.py
+++ dalec_caldav/proxy.py
@@ -12,10 +12,10 @@ from dalec.proxy import Proxy
 from caldav import DAVClient, dav
 
 client = DAVClient(
-        url=settings.DALEC_CALDAV_BASE_URL,
-        username=settings.DALEC_CALDAV_API_USERNAME,
-        password=settings.DALEC_CALDAV_API_PASSWORD,
-        )
+    url=settings.DALEC_CALDAV_BASE_URL,
+    username=settings.DALEC_CALDAV_API_USERNAME,
+    password=settings.DALEC_CALDAV_API_PASSWORD,
+)
 
 
 class CaldavProxy(Proxy):
@@ -31,15 +31,13 @@ class CaldavProxy(Proxy):
         if content_type == "event":
             return self._fetch_event(nb, channel, channel_object)
 
-        raise ValueError(f"Invalid content_type {content_type}. Accepted: event." )
+        raise ValueError(f"Invalid content_type {content_type}. Accepted: event.")
 
     def _fetch_event(self, nb, channel=None, channel_object=None):
         """
         Get latest event from calendar
         """
-        options = {
-                "per_page": nb,
-                }
+        options = {"per_page": nb}
 
         if channel == "url":
             if channel_object[-1] != "/":
@@ -59,16 +57,15 @@ class CaldavProxy(Proxy):
             for event in calendar.events():
                 id = str(event.vobject_instance.vevent.contents["uid"][0].value)
                 content = {
-                        key: val[0].value
-                        for key, val
-                        in event.vobject_instance.vevent.contents.items()
-                        }
+                    key: val[0].value
+                    for key, val in event.vobject_instance.vevent.contents.items()
+                }
                 content["event_url"] = str(event.url)
                 content["dav_calendar_url"] = calendar_url
                 content["calendar_displayname"] = calendar_displayname
 
                 res = urlparse(calendar.url.url_raw)
-                if ( "/remote.php/dav/public-calendars/" in res.path):
+                if "/remote.php/dav/public-calendars/" in res.path:
                     # seems to be nextcloud
                     token = res.path.split("/")[-2]
                     nextcloud_calendar_url = "{}://{}/index.php/apps/calendar/p/{}".format(
@@ -77,9 +74,9 @@ class CaldavProxy(Proxy):
                     content["nextcloud_calendar_url"] = nextcloud_calendar_url
 
                 contents[id] = {
-                        **content,
-                        "id": id,
-                        "creation_dt": content["created"],
-                        "last_update_dt": now()
-                        }
+                    **content,
+                    "id": id,
+                    "creation_dt": content["created"],
+                    "last_update_dt": now(),
+                }
         return contents
